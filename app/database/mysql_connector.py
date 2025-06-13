109dc413f722abbce5d4f19f2ec18b41574b5e00
Angel <113852397+AngelOncoy@users.noreply.github.com>
2025-06-13 10:54:55 -0500

    feat: base de datos desplegada

--- app/database/mysql_connector.py
+++ app/database/mysql_connector.py
@@ -1,22 +1,28 @@
 import mysql.connector
 from mysql.connector import Error
 
+
 def get_connection():
     """
-    Obtiene la conexión a la base de datos MySQL.
+    Obtiene la conexión a la base de datos MySQL en Railway.
     """
     try:
+        # Configuración de conexión usando el MYSQL_PUBLIC_URL de Railway
         connection = mysql.connector.connect(
-            host="localhost",  # Cambia esto si tu base de datos está en otro host
-            user="root",  # Cambia con tu usuario
-            password="123456",  # Cambia con tu contraseña
-            database="rf_db"  # Cambia con el nombre de tu base de datos
+            host="crossover.proxy.rlwy.net",  # Host proporcionado por Railway
+            user="root",  # Usuario proporcionado por Railway
+            password="XWlocoFTcvsjYhEWLePLYNCqFtjGywBt",  # Contraseña proporcionada por Railway
+            database="railway",  # Nombre de la base de datos en Railway
+            port=28279  # Puerto proporcionado por Railway
         )
+
         if connection.is_connected():
+            print("Conexión exitosa a la base de datos Railway.")
             return connection
     except Error as e:
         print(f"Error de conexión: {e}")
         return None
+
 
 def close_connection(connection):
     """
@@ -24,3 +30,4 @@ def close_connection(connection):
     """
     if connection and connection.is_connected():
         connection.close()
+        print("Conexión cerrada.")
